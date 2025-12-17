ac0527fc344aefe22db82c94cff259f02b199a53
bentdixon <btdixon@emory.edu>
2025-12-17 16:28:41 -0500

    Fixed strange cuDNN error by reducing very large text sizes for language ID

--- utils/determine_language.py
+++ utils/determine_language.py
@@ -4,10 +4,19 @@ from stanza.pipeline.core import Pipeline
 from utils.transcripts import Transcript
 from data.langs import Language
 
-nlp = Pipeline(lang="multilingual", processors="langid")
+nlp = Pipeline(lang="multilingual", processors="langid", use_gpu=True)
 
 def determine_language(transcript: Transcript) -> Language:
-    text = "\n".join(line.text for line in transcript.lines)
+    if len(transcript.lines) > 450:  # prevents strange cuDNN error
+        text = "\n".join(line.text for line in transcript.lines[:450])
+        print("Reducing text size...")
+    else:
+        text = "\n".join(line.text for line in transcript.lines)
+
+    if len(text) == 0:
+        print(f"Transcript has length of 0: {transcript.filename}, returning LANGUAGE.UNKNOWN")
+        return Language.UNKNOWN
+
     doc = cast(Document, nlp(text))
     try:
         if doc.lang:
